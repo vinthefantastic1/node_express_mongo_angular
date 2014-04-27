e0dbfd8771ba3f5f31a78c5c457eefda6bad288b
Vinny2020 <vincentl2020@gmail.com>
2014-04-27 11:32:39 -0400

    code cleanup

--- plunetController.js
+++ plunetController.js
@@ -12,64 +12,13 @@ plunetApp.controller("PlunetController", function ($scope, plunetFactory) {
 
     plunetFactory.getPlunetAsync(function (results) {
         console.log('plunetController async returned value');
-      //  console.log(results);
+        console.log(results);
         $scope.plunetJobs = results;
     });
 
     $scope.understand = "I now understand how the scope works! Put anything into the box.."
     $scope.inputValue = "";
 
-
-    $scope.selectedPerson = 0;
-    $scope.selectedGenre = null;
-    $scope.PlunetJobs = null;
-
-    $scope.people = [
-        {
-            id: 0,
-            name: 'Leon',
-            music: [
-                'Rock',
-                'Metal',
-                'Dubstep',
-                'Electro'
-            ],
-            live: true
-        },
-        {
-            id: 1,
-            name: 'Chris',
-            music: [
-                'Indie',
-                'Drumstep',
-                'Dubstep',
-                'Electro'
-            ],
-            live: true
-        },
-        {
-            id: 2,
-            name: 'Harry',
-            music: [
-                'Rock',
-                'Metal',
-                'Thrash Metal',
-                'Heavy Metal'
-            ],
-            live: false
-        },
-        {
-            id: 3,
-            name: 'Allyce',
-            music: [
-                'Pop',
-                'RnB',
-                'Hip Hop'
-            ],
-            live: true
-        }
-    ];
-
-
+//    $scope.PlunetJobs = null;
 
 });
